39180de0394b4577c4cb0c8ec4182f0cedda9238
Nick Barnette <nicdavid@live.unc.edu>
2021-01-13 22:00:16 -0500

    adding run runner

--- src/index.py
+++ src/index.py
@@ -3,22 +3,24 @@ from Player import Player
 from Card import Card
 
 
-g = Game()
-g.deal()
-
-print(g.get_house())
-for p in range(len(g.players)):
-    print(g.get_player(p))
-print(g.find_outcome())
+c1 = Card(rank="Ace", suit="Diamond")
+c2 = Card(rank="Ace", suit="Spade")
+PLAYER = Player(id=0, hand=[c1, c2])
+HANDS = 10000
 
 
-print()
-p1 = Player(
-    id=0, hand=[Card(rank="King", suit="Diamond"), Card(rank="Queen", suit="Spade")]
-)
-g = Game(player=p1)
-g.deal()
-print(g.get_house())
-for p in range(len(g.players)):
-    print(g.get_player(p))
-print(g.find_outcome())
+def run_hands():
+    wins = 0
+    for i in range(HANDS):
+        g = Game(player=PLAYER)
+        g.deal()
+        if g.find_outcome()[0]["id"] == 0:
+            wins += 1
+    print(
+        "Win Percent [%s %s]: %d"
+        % (str(c1), str(c2), round(wins / HANDS * 10000) / 100)
+        + "%"
+    )
+
+
+run_hands()
